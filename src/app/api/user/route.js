c66742630b7a59279bc0a54e5077c27b4a34e216
talha-fullstackdev <talhafullstack786@gmail.com>
2025-06-25 14:00:01 +0500

    formated code in api

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,13 +1,11 @@
-// src/app/api/user/route.js
-import prisma from '@/lib/prisma'; // or use relative path if @ alias not set
+import prisma from "@/lib/prisma";
 
 export async function GET(request) {
   const users = await prisma.user.findMany();
-  return Response.json({users,success:true});
+  return Response.json({ users, success: true });
 }
 
 export async function POST(request) {
-
   const body = await request.json();
   const { name, email } = body;
 
@@ -15,5 +13,5 @@ export async function POST(request) {
     data: { name, email },
   });
 
-  return Response.json({user, success:true,status: 201});
+  return Response.json({ user, success: true, status: 201 });
 }
